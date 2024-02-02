6482954dfb675e47689a46c0d1e3822b033ad0c9
Erik <mariglianoerik@gmail.com>
2024-02-02 15:46:52 +0100

    Primo esercizio

---
+++ src/main/java/Springboot/exercises/Ex1/ExercisesApplication.java
@@ -0,0 +1,13 @@
+package Springboot.exercises.Ex1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExercisesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExercisesApplication.class, args);
+	}
+
+}
